0211205cfc6f29a1c15e95517526588a082290c9
ImExoOdeex <odeex722@gmail.com>
2022-11-10 02:13:48 +0100

    pg-listen notify

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+import { WebhookClient } from "discord.js"
+import createPostgresSubscriber from "pg-listen"
+
 require('dotenv').config()
 const express = require("express")
 const app = express()
@@ -13,21 +16,25 @@ const client = new Client({
     database: "sklepik"
 })
 
-const webhook = new Discord.WebhookClient("")
-// https://discord.com/api/webhooks/1040035942553092207/RbQUP-Cddx6Z0xMDglKOmB4B8u6E0yrDY2vX8VkbamOQeQVV3Uce9fkB1E5G-TzT8oTr
+const webhook = new WebhookClient({ id: "1040035942553092207", token: "RbQUP-Cddx6Z0xMDglKOmB4B8u6E0yrDY2vX8VkbamOQeQVV3Uce9fkB1E5G-TzT8oTr" })
 
-client.connect((err: any, client: any, done: any) => {
-    if (err) {
-        console.error("error");
-    } else {
-        console.log("connected to db");
-        client.on("notification", (msg: any) => {
-            console.log(msg.payload);
-        })
-        const query = client.query("LISTEN update_notification")
-    }
+const subscriber = createPostgresSubscriber({ connectionString: process.env.DB_URL })
+
+connect()
+
+subscriber.notifications.on("Order", (payload) => {
+    // Payload as passed to subscriber.notify() (see below)
+    console.log("Received notification in 'Order':", payload)
 })
 
-app.use("/", async (req: any, res: any) => {
-    res.send("dawda")
-})
+export async function connect() {
+    await subscriber.connect()
+    await subscriber.listenTo("Order")
+}
+
+export async function sendSampleMessage() {
+    await subscriber.notify("Order", {
+        greeting: "Hey, buddy.",
+        timestamp: Date.now()
+    })
+}
